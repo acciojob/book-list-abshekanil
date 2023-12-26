78842702d88e4440084db3457d2702ed5d74d174
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-26 13:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+let loginForm = document.querySelector(".form-group form");
+let table = document.getElementById("book-list");
+
+loginForm.addEventListener("submit" , (e) =>{
+	e.preventDefault();
+
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbn = document.getElementById("isbn").value;
+
+	
+	let newRow = table.insertRow(table.rows.length);
+
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+	let cell3 = newRow.insertCell(2);
+	let cell4 = newRow.insertCell(3);
+
+	cell1.innerHTML = title;
+	cell2.innerHTML = author;
+	cell3.innerHTML = isbn;
+
+	let cancelButton = document.createElement("button");
+	cancelButton.textContent = "X";
+	cancelButton.classList.add("delete");
+
+	cancelButton.addEventListener("click" , function(){
+		table.deleteRow(newRow.rowIndex);
+	});
+	cell4.appendChild(cancelButton);
+});
+
+table.addEventListener("click" , function(e){
+	if(e.target.classList.contains("delete"))
+	{
+		let row = e.target.parentNode.parentNode;
+		if(row)
+		{
+			table.deleteRow(row.rowIndex);
+		}
+	}
+});
